2b87f904bf8e4800c7c23cf4696febdca1a94377
estevanmontoya <montoya.estevan@gmail.com>
2017-05-30 21:44:12 -0600

    Added view engine and public directory

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
 app.get('/', (req, res) => {
-  res.send('Page coming soon');
+  res.render('index');
 })
 
 app.listen('8080', () => {
